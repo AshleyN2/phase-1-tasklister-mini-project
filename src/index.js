c2af2644d89d57eddbb2add707d76b2ae0dfbbf0
Ashley <ashleynjeri7@gmail.com>
2022-08-24 18:06:41 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let taskForm = document.querySelector('form')
+  taskForm.addEventListener('submit', (e) => { // Enables user to click submit form
+    e.preventDefault();
+    buildTaskLister(e.target.new_task.value)
+    taskForm.reset()
+  })
 });
+
+// Enables user to type a task and it gets displayed
+
+function buildTaskLister(newTask){
+  let btn = document.createElement('button')
+  btn.textContent = 'X'
+  btn.addEventListener('click', handleDelete)
+  let p = document.createElement('p')
+  p.textContent = `${newTask } `
+  document.querySelector('#tasks').appendChild(p)
+  p.append(btn)
+  
+}
+
+// Enables user to delete a task
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
